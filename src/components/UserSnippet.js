d9a101bce88abcd4ca862abcf777f62e91148ece
maaislam <maaislam@outlook.com>
2020-07-25 09:03:27 +0600

    user image conditional render

--- src/components/UserSnippet.js
+++ src/components/UserSnippet.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 const UserSnippet = (props) => {
-    return (
+
+    if(props.userImg){
+        return (
         <div className="item">
             <img className="ui avatar image" src= {props.userImg} alt = "user"/>
             <div className="content">
                 <div className="header">{props.name}</div>
             </div>
         </div>
-    );
+        );
+    
+    } else {
+        return (
+            <div className="item">
+                <i className="user circle blue large icon"></i>
+                <div className="content">
+                    <div className="header">{props.name}</div>
+                </div>
+            </div>
+        );
+    }
+
+    
+    
 }
 
 export default UserSnippet;
